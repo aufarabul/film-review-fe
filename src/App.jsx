1a32d2a0509dd5cfa6b493bc31dd2c80661fe6a4
aufarabul <aufarabul@gmail.com>
2024-06-25 16:28:02 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import { Provider } from "react-redux";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import "./App.css";
-
+import store from "./redux/store";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <Navbar>
-        <HomePage />
-      </Navbar>
-    ),
-  },
-  {
-    path: "/login",
-    element: (
-      <>
-        <LoginPage />
-      </>
-    ),
-  },
-]);
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ]);
 
-export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    // We will use the store (temporary database)
+    <Provider store={store}>
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
+
+      <ToastContainer theme="colored" />
+    </Provider>
+  );
 }
+
+export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,26 @@
-import "../App.css";
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
+// Initialization connect to backend websocket (socket.io)
 
-export default function HomePage() {
-  return <div>hallo bang</div>;
+function Home() {
+  return (
+    <>
+      <Row className="mt-4">
+        <Col>
+          <h6>{typing && "seseorang sedang mengetik...."}</h6>
+        </Col>
+      </Row>
+
+      <Row className="mt-4">
+        <Col>
+          {messages?.length > 0 &&
+            messages?.map((message) => (
+              <MessageItem data={message} key={message.id} />
+            ))}
+        </Col>
+
+        <AddMessage socket={socket} />
+      </Row>
+    </>
+  );
 }
+
+export default Home;
